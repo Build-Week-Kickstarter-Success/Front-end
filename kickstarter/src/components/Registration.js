6868d8bc2ba6982847f925505c229b30a81178ba
cyasmani <yasmanicastaneda@yahoo.com>
2020-08-20 20:21:27 -0700

    added functionality to button and created changehandler

--- kickstarter/src/components/Registration.js
+++ kickstarter/src/components/Registration.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "../styles/registration.css";
 import * as Yup from "yup";
 
 function Registration() {
+    const [buttonDisabled, setButtonDisabled] = useState(true)
     const [form, setForm] = useState({
         firstname: "",
         lastname: "",
@@ -11,6 +12,15 @@ function Registration() {
         password: ""
 
     });
+
+    const [error, setError] = useState({
+        firstname: "",
+        lastname: "",
+        username:"",
+        email: "",
+        password: ""
+
+    })
 
     const formSchema = Yup.object().shape({
         email: Yup.string().email("Must be a valid email address")
@@ -22,6 +32,30 @@ function Registration() {
 
     })
 
+    const formChange = event => {
+        setForm({
+            ...form, [event.target.name] : event.target.value
+        })
+    }
+
+    const validateChange = event => {
+
+    }
+
+    useEffect(() => {
+        formSchema.isValid(form).then(isValid => {
+            setButtonDisabled(!isValid)
+        })
+    
+    }, [form])
+
+    const formSubmit = event => {
+        event.preventDefault();
+
+    }
+
+    
+
     return (
         <div>
             <div className="maincontainer">
@@ -30,29 +64,32 @@ function Registration() {
             </div>
             <div className="container"> 
                 <div className="formcontainer">
-                    <form>
+                    <form onSubmit={formSubmit}>
                         <br></br>
                         <label>
                             First Name
-                            <input type="text" name="firstname"/>
+                            <input type="text" name="firstname" onChange={formChange} value={form.firstname}/>
                         </label>
                         <label>
                             Last Name
-                            <input type="text" name="lastname"/>
+                            <input type="text" name="lastname"
+                            onChange={formChange} value={form.lastname}/>
                         </label>
                         <label>
                             Email
-                            <input type="text" name="email"/>
+                            <input type="text" name="email"
+                            onChange={formChange} value={form.email}/>
                         </label>
                         <label>
                             Username
-                            <input type="text" name="username" />
+                            <input type="text" name="username" 
+                            onChange={formChange} value={form.username}/>
                         </label>
                         <label>
                             Password
-                            <input type="password" name="password"/>
+                            <input type="password" name="password" onChange={formChange} value={form.password}/>
                         </label>
-                        <button type="submit">Register</button>
+                        <button disabled={buttonDisabled}type="submit">Register</button>
 
                     </form>
                 </div>
